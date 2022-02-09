3bbc2891f93cf1f898e746520de023b42675519b
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-09 14:56:22 +0100

    green

--- ShoppingBasketKata/ShoppingBasketKata/ShoppingBasketRepository.cs
+++ ShoppingBasketKata/ShoppingBasketKata/ShoppingBasketRepository.cs
@@ -2,18 +2,33 @@
 
 public class ShoppingBasketRepository : IShoppingBasketRepository
 {
-    private readonly List<UserProductAmount> database = new();
+    private readonly Dictionary<UserID, Dictionary<ProductID, int>> database = new();
 
-    public void Register(UserID userID, ProductID productID, int amount) =>
-        database.Add(new UserProductAmount(productID, userID, amount));
+    public void Register(UserID userID, ProductID productID, int amount)
+    {
+        if (!database.ContainsKey(userID))
+        {
+            database[userID] = new Dictionary<ProductID, int>();
+        }
+
+        if (!database[userID].ContainsKey(productID))
+        {
+            database[userID][productID] = amount;
+            return;
+        }
+
+        database[userID][productID] += amount;
+    }
 
     public UserProductAmount[] GetFor(UserID userID)
     {
+        if (!database.ContainsKey(userID))
+            return Array.Empty<UserProductAmount>();
+
         var result = new List<UserProductAmount>();
-        foreach (var item in database)
+        foreach (var item in database[userID])
         {
-            if (item.UserID == userID)
-                result.Add(item);
+            result.Add(new UserProductAmount(item.Key, userID, item.Value));
         }
 
         return result.ToArray();
