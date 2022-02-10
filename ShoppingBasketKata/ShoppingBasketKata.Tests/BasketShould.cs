5069ce9cd0f6f1690ec7757311632bf41f6e397d
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-10 12:55:40 +0100

    green-ish

--- ShoppingBasketKata/ShoppingBasketKata.Tests/BasketShould.cs
+++ ShoppingBasketKata/ShoppingBasketKata.Tests/BasketShould.cs
@@ -6,7 +6,7 @@ namespace ShoppingBasketKata.Tests;
 public class BasketShould
 {
     private static readonly Product OneCentProduct =
-        new Product("One Cent, forty nine times less than the rapper", new Money(1), new ProductID(3));
+        new("One Cent, forty nine times less than the rapper", new Money(1), new ProductID(3));
 
     [Fact(DisplayName = "calculate the total for one product")]
     public void Test1()
@@ -17,5 +17,16 @@ public class BasketShould
             new[] { new BasketEntry(OneCentProduct, 1) }
         );
         Assert.Equal(new Money(1), basket.TotalAmount);
+    }
+
+    [Fact(DisplayName = "calculate the total for one product three times")]
+    public void Test2()
+    {
+        var basket = new Basket(
+            new UserID(1),
+            DateTime.Now,
+            new[] { new BasketEntry(OneCentProduct, 3) }
+        );
+        Assert.Equal(new Money(3), basket.TotalAmount);
     }
 }
--- ShoppingBasketKata/ShoppingBasketKata/Basket.cs
+++ ShoppingBasketKata/ShoppingBasketKata/Basket.cs
@@ -2,5 +2,15 @@
 
 public record Basket(UserID UserID, DateTime CreatedAt, BasketEntry[] ExpectedProducts)
 {
-    public Money TotalAmount => throw new NotImplementedException();
+    public Money TotalAmount
+    {
+        get
+        {
+            var result = 0;
+            foreach (var expectedProduct in ExpectedProducts)
+                result += expectedProduct.Amount * expectedProduct.Product.Price.Amount;
+
+            return new Money(result);
+        }
+    }
 }
